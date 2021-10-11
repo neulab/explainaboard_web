76e5394c4ed14c1b4d54d171f0ca6a87e9a4de12
OscarWang114 <chihhaowang.oscar@gmail.com>
2021-10-11 16:22:40 -0400

    Separate codegen and custom implementation

--- backend/gen.sh
+++ backend/gen.sh
@@ -1,16 +1,23 @@
 # reference: https://stackoverflow.com/a/47554626
 
+# cd to the directory gen.sh lives in
+cd "$(dirname "${BASH_SOURCE[0]}")" 
+
+# download codegen cli if not exists
 if [ ! -f swagger-codegen-cli-3.0.29.jar ]; then
     wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.29/swagger-codegen-cli-3.0.29.jar -O swagger-codegen-cli-3.0.29.jar
 fi
 
-mkdir -p src-gen/explainaboard && \
-cd src-gen/explainaboard/ && \
-ln -sf src/server_impl/ && \
-cd ../../ && \
+# remove src/gen if exists and generate code
+# we also create a link to src/impl in src/gen which contains our own implementation 
+rm -rf src/gen && \
+mkdir -p src/gen/explainaboard && \
+cd src/gen/explainaboard/ && \
+ln -sf ../../impl/ && \
+cd ../../../ && \
 java -jar swagger-codegen-cli-3.0.29.jar generate \
     -i ../src/openapi.yaml \
     -l python-flask \
-    -o src-gen \
+    -o src/gen \
     -t templates \
-    -DpackageName=explainaboard
+    -c swagger-codegen-config.json
