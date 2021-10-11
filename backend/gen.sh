35d2cd9f583243bd6f001426451bee89504bed69
OscarWang114 <chihhaowang.oscar@gmail.com>
2021-10-11 14:43:10 -0400

    Add codegen jar

--- backend/gen.sh
+++ backend/gen.sh
@@ -1,10 +1,14 @@
 # reference: https://stackoverflow.com/a/47554626
 
+if [ ! -f swagger-codegen-cli-3.0.29.jar ]; then
+    wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.29/swagger-codegen-cli-3.0.29.jar -O swagger-codegen-cli-3.0.29.jar
+fi
+
 mkdir -p src-gen/explainaboard && \
 cd src-gen/explainaboard/ && \
 ln -sf src/server_impl/ && \
 cd ../../ && \
-java -jar swagger-codegen-cli.jar generate \
+java -jar swagger-codegen-cli-3.0.29.jar generate \
     -i ../src/openapi.yaml \
     -l python-flask \
     -o src-gen \
