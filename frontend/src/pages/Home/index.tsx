bdff21eaecacfd7adc74d417dccdf58c19a7aeda
OscarWang114 <chihhaowang.oscar@gmail.com>
2021-11-16 16:47:17 -0500

    Home/index.tsx: Request system instead of dataset

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -4,11 +4,13 @@ import logo from "../../logo.svg";
 import "./index.css";
 
 export function Home() {
-  const [datasets, setDatasets] = useState<string>();
+  const [system, setSystem] = useState<string>();
   useEffect(() => {
     async function init() {
-      const data = await backendClient.datasetsDatasetIdGet(11);
-      setDatasets(JSON.stringify(data));
+      const data = await backendClient.systemsSystemIdGet(
+        "6178746a42455b0303bd8d09"
+      );
+      setSystem(JSON.stringify(data));
     }
     init();
   }, []);
@@ -17,8 +19,8 @@ export function Home() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>ExplainaBoard</p>
-        <p>(requesting datasets... )</p>
-        <div>{datasets}</div>
+        <p>(requesting system id 6178746a42455b0303bd8d09... )</p>
+        <div>{system}</div>
         <a
           className="App-link"
           href="http://explainaboard.nlpedia.ai/"
