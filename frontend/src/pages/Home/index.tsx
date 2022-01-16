46c9d7fd4b5061485ea5e5e0b0d80bfc1d97bb1b
OscarWang114 <chihhaowang.oscar@gmail.com>
2022-01-16 16:12:42 -0500

    Clean Homepage

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -4,30 +4,18 @@ import logo from "../../logo.svg";
 import "./index.css";
 
 export function Home() {
-  const [systemMetadata, setSystemMetadata] = useState<string>();
-  useEffect(() => {
-    async function init() {
-      const data = await backendClient.systemsSystemIdGet(
-        "61b02e0baadd2f674845b1c2"
-      );
-      setSystemMetadata(JSON.stringify(data));
-    }
-    init();
-  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>ExplainaBoard</p>
-        <p>(Request system id 61b02e0baadd2f674845b1c2:)</p>
-        <div>{systemMetadata}</div>
         <a
           className="App-link"
           href="http://explainaboard.nlpedia.ai/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          ExplainaBoard
+          Previous ExplainaBoard
         </a>
       </header>
     </div>
