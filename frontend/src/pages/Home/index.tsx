fce24b330fe583d245fc2886dfef15ef1da14086
OscarWang114 <chihhaowang.oscar@gmail.com>
2022-02-09 17:28:44 -0500

    Tailwind CSS, logo, homepage

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,24 +1,38 @@
 import React from "react";
-import { Typography } from "antd";
 
-import logo from "../../logo.svg";
+import logo from "../../logo-full.png";
 import "./index.css";
 
 export function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Typography.Title>ExplainaBoard</Typography.Title>
-        <a
-          className="App-link"
-          href="http://explainaboard.nlpedia.ai/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ExplainaBoard
-        </a>
-      </header>
+    <div className="h-full container flex flex-wrap items-center">
+      <div
+        className="w-full h-4/5 bg-center bg-cover"
+        style={{ backgroundImage: `url(${logo})` }}
+      >
+        <div className="w-full h-full bg-opacity-60 bg-black items-center flex">
+          <div className="text-center ml-auto mr-auto">
+            <h1 className="text-white text-6xl">ExplainaBoard</h1>
+            <p className="text-white text-2xl ">
+              An Explainable Leaderboard for NLP
+            </p>
+          </div>
+        </div>
+        ˜
+      </div>
+      <div className="text-center ml-auto mr-auto px-20">
+        <h2 className="text-3xl">Do you want to?</h2>
+        <ul className="list-disc text-xl text-left">
+          <li>
+            Upload and analyze your own system results on an existing dataset.
+          </li>
+          <li>View a list of supported datasets.</li>
+          <li>
+            View well-performing systems on a particular dataset, and
+            compare/contrast their strengths and weaknesses.
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
