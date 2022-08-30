de6f3c52b40270b3ac9eed7bb0c99a03658ed084
Lyuyang Hu <lyuyangh@andrew.cmu.edu>
2022-08-29 23:06:08 -0400

    replace flask dev server with gunicorn in prod (#270)
    * replace flask dev server with gunicorn in prod
    
    * pin npm version in Dockerfile

---
+++ deployment/serve.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Starts nginx and gunicorn. It is meant for the explainaboard_web
+# container only. The script takes any number of gunicorn arguments
+# passed in as a string. 
+
+nohup nginx -g "daemon off;" & gunicorn -b 0.0.0.0:5000 "explainaboard_web.__main__:create_app()" $1
