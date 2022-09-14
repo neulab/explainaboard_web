d8e779335c2e1a303784b8fc58235e9ba040b09b
qjiang002 <jiangqi@jiangs-mbp.myfiosgateway.com>
2022-09-13 22:16:22 -0400

    filter out languages without iso639-3 codes

--- backend/src/impl/language_code.py
+++ backend/src/impl/language_code.py
@@ -20,14 +20,11 @@ _additional_language_codes: list[LanguageCode] = [
 
 def get_language_codes():
     """getter for language codes data"""
-    language_codes = []
-    for lang in list(languages):
-        if len(lang.part3) > 0:
-            language_codes.append(LanguageCode(lang.name, lang.part1, lang.part3))
-        else:
-            language_codes.append(
-                LanguageCode(lang.name, lang.part1, "unkown")
-            )  # some languages don't have iso369-3 code, use "unknown"
+    language_codes = [
+        LanguageCode(lang.name, lang.part1, lang.part3)
+        for lang in list(languages)
+        if len(lang.part3) > 0
+    ]
     language_codes.sort(key=lambda x: (x.iso3_code, x.name))
     language_codes += _additional_language_codes
     return language_codes
