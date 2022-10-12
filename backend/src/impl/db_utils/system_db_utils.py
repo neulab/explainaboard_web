44878242c2f7b593c7e844233c02f4f3b3869970
Lyuyang Hu <lyuyangh@andrew.cmu.edu>
2022-10-11 22:37:48 -0400

    handle optional values properly in model classes (#395)
    * handle optional values properly in model classes
    
    * Apply suggestions from code review
    
    Co-authored-by: Yusuke Oda <yusuke.oda@predicate.jp>
    Co-authored-by: Tetsuo Kiso <tetsuok@users.noreply.github.com>
    
    * reflect comments
    
    Co-authored-by: Yusuke Oda <yusuke.oda@predicate.jp>
    Co-authored-by: Tetsuo Kiso <tetsuok@users.noreply.github.com>

--- backend/src/impl/db_utils/system_db_utils.py
+++ backend/src/impl/db_utils/system_db_utils.py
@@ -102,6 +102,23 @@ class SystemDBUtils:
             metric_stats = document["metric_stats"]
             document["metric_stats"] = []
 
+        # FIXME(lyuyang): The following for loop is added to work around an issue
+        # related to default values of models. Previously, the generated models
+        # don't enforce required attributes. This function exploits that loophole.
+        # Now that we have fixed that loophole, this function needs some major
+        # refactoring. None was assigned for these fields before implicitly. Now
+        # we assign them explicitly so this hack does not change the current
+        # behavior.
+        for required_field in (
+            "created_at",
+            "last_modified",
+            "system_id",
+            "system_info",
+            "metric_stats",
+        ):
+            if required_field not in document:
+                document[required_field] = None
+
         system = System.from_dict(document)
         if include_metric_stats:
             # Unbinarize to numpy array and set explicitly
