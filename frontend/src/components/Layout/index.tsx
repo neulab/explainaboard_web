fb5a814133f58cb0e5be80a9b9c0ae3c1d623e2f
qjiang002 <jiangqi@jiangs-mbp-2.myfiosgateway.com>
2022-10-31 15:55:46 -0400

    remove unused icon and add homepage API button

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,16 +1,12 @@
 import React, { ReactNode, useState } from "react";
-import { Dropdown, Layout as AntdLayout, Menu, Typography } from "antd";
+import { Layout as AntdLayout, Menu } from "antd";
 import { Route } from "../../routes";
 import "./index.css";
 import logo from "../../logo-simple.png";
 import { useHistory, useLocation } from "react-router";
 import { useEnv } from "..";
 import { UserPanel } from "../UserPanel";
-import {
-  TwitterCircleFilled,
-  GithubFilled,
-  FacebookFilled,
-} from "@ant-design/icons";
+import { GithubFilled } from "@ant-design/icons";
 
 interface Props {
   routes: Route[];
@@ -47,35 +43,6 @@ export const Layout: React.FC<Props> = ({ routes, children }) => {
     else setOpenMenus([...openMenus, menuKey]);
   };
 
-  const githubMenu = (
-    <Menu>
-      <Menu.Item key="SDK">
-        <Typography.Link
-          href={"https://github.com/neulab/ExplainaBoard"}
-          target="_blank"
-        >
-          SDK repo
-        </Typography.Link>
-      </Menu.Item>
-      <Menu.Item key="CLI">
-        <Typography.Link
-          href={"https://github.com/neulab/explainaboard_client"}
-          target="_blank"
-        >
-          Client repo
-        </Typography.Link>
-      </Menu.Item>
-      <Menu.Item key="WEB">
-        <Typography.Link
-          href={"https://github.com/neulab/explainaboard_web"}
-          target="_blank"
-        >
-          Web UI repo
-        </Typography.Link>
-      </Menu.Item>
-    </Menu>
-  );
-
   return (
     <AntdLayout style={{ minHeight: "100vh" }}>
       <AntdLayout.Sider collapsible collapsed={collapsed} onCollapse={toggle}>
@@ -89,6 +56,7 @@ export const Layout: React.FC<Props> = ({ routes, children }) => {
             key={"logo"}
             icon={<img src={logo} alt="" style={{ height: "20px" }} />}
             onClick={() => history.push("/")}
+            style={{ height: "44px" }}
           >
             <div className="site-title">ExplainaBoard</div>
           </Menu.Item>
@@ -134,17 +102,16 @@ export const Layout: React.FC<Props> = ({ routes, children }) => {
         <AntdLayout.Header className="site-layout-header">
           <div className="header-items-container">
             <div className="header-icon">
-              <Dropdown overlay={githubMenu} placement="bottom">
-                <div>
-                  <GithubFilled />
-                </div>
-              </Dropdown>
-            </div>
-            <div className="header-icon">
-              <FacebookFilled />
-            </div>
-            <div className="header-icon">
-              <TwitterCircleFilled />
+              <div>
+                <GithubFilled
+                  onClick={() =>
+                    window.open(
+                      "https://github.com/neulab/ExplainaBoard",
+                      "_blank"
+                    )
+                  }
+                />
+              </div>
             </div>
             <div style={{ paddingRight: "10px" }}>
               {env === "development" && "(development environment)"}
