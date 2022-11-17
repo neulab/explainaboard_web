4e1aa9dcb104daf26a598b8fc3efa408aec27c4b
qjiang002 <jiangqi@jiangs-mbp-2.wifi.local.cmu.edu>
2022-11-17 16:04:24 -0500

    fix import

--- frontend/src/components/SystemsTable/SystemTableTools.tsx
+++ frontend/src/components/SystemsTable/SystemTableTools.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {
-  // Button,
-  // ButtonProps,
+  Button,
+  ButtonProps,
   Input,
   Select,
   Space,
@@ -21,7 +21,6 @@ import { SystemModel } from "../../models";
 import { LoginState, useUser } from "../useUser";
 import { backendClient, parseBackendError } from "../../clients";
 import { FilterUpdate, SystemFilter } from "./SystemFilter";
-import Button, { ButtonProps } from "antd-button-color";
 
 interface Props {
   systems: SystemModel[];
